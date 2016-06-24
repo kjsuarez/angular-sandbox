ef0bcf76864c3dbdf6bc57ae202f1a27e88785fd
Kevin <supergoodblog@gmail.com>
2016-06-23 20:02:13 -0400

    further fumbling

--- sandbox.js
+++ sandbox.js
@@ -4,12 +4,22 @@ angular.module('sandbox',[
 	$scope.hello = 'world';
 
 	$scope.things = [
-		{"id":0, "name":"box", "cool?":"no", "traits":["square","3D"]},
-		{"id":1, "name":"paper crane", "cool?":"yes", "traits":["pretty", "imbued with esoteric might"]},
-		{"id":2, "name":"so sad", "cool?":"nope", "traits":["I'd rather not talk about it"]}
+		{"id":0, "name":"box", "cool":"no", "traits":["square","3D"]},
+		{"id":1, "name":"paper crane", "cool":"yes", "traits":["pretty", "imbued with esoteric might"]},
+		{"id":2, "name":"so sad", "cool":"nope", "traits":["I'd rather not talk about it"]}
 	]
 
 	$scope.coolestThing = null;
+
+	$scope.newThing = {
+		"name":'',
+		"cool":'',
+		"traits":[]	
+	};
+	$scope.thingFormTraits = [];
+	$scope.newTrait = {
+		"body":''
+	};
 
 	function anythingCool () {
 		return $scope.coolestThing !== null
@@ -23,7 +33,85 @@ angular.module('sandbox',[
 		return $scope.coolestThing !== null && thing.name === $scope.coolestThing.name;		
 	}
 
-	$scope.anythingCool = anythingCool;
-	$scope.setCoolestThing = setCoolestThing; //no () important
+	function noCool () {
+		$scope.coolestThing = null
+	}
+
+	$scope.anythingCool = anythingCool;  //no () important
+	$scope.setCoolestThing = setCoolestThing; 
 	$scope.isCoolestThing = isCoolestThing;
+	$scope.noCool = noCool;
+
+	$scope.isCreating = false;
+	$scope.isEditing = false;
+	$scope.createDropDown = false;
+	$scope.editDropDown = false;
+
+
+	function canCreate () {
+		return !anythingCool()
+	}
+
+	function canEdit () {
+		return anythingCool()
+	}
+
+	function resetCreateForm () {
+		$scope.newThing = {
+		"name":'',
+		"cool":'',
+		"traits":[]	
+		};
+
+		$scope.newTrait = {
+			"body":''
+		};
+
+		$scope.thingFormTraits = [];
+	}
+
+
+	function resetTraitForm () {
+		$scope.newTrait = {
+			"body":''
+		};
+
+	}
+
+	function switchCreate () {
+		$scope.createDropDown = !$scope.createDropDown;
+	}
+
+	function switchEdit () {
+		$scope.editDropDown = !$scope.editDropDown;
+	
+	}
+
+	function createThing (thing) {
+		x = {"id": $scope.things.length, "name":thing.name, "cool":thing.cool, "traits":$scope.thingFormTraits}
+		$scope.things.push(x);
+		resetCreateForm();
+	}
+
+
+	function addTraits (trait) {
+		$scope.thingFormTraits.push(trait.body)
+		resetTraitForm();
+	}
+
+	$scope.canCreate = canCreate;
+	$scope.canEdit = canEdit;
+
+	$scope.createThing = createThing;
+	
+	$scope.addTraits = addTraits;
+
+	$scope.switchCreate = switchCreate;
+	$scope.switchEdit = switchEdit;
 });
+
+
+
+
+
+
